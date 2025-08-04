04f463190f10dc714efdaba2a61fe868b5df07ec
Sakib008 <mdshakib1902@gmail.com>
2025-08-04 21:22:04 +0530

    fix : watermark enable

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This disables the Next.js watermark in development mode
-  devIndicators: {
-    buildActivity: true,
-    // buildActivityPosition: "bottom-right",
-  },
+ 
   images: {
     domains: ["images.pexels.com",'drive.google.com'],
   },
