b58f9e478579e23e98e8e7e74243ea2215bc0dd4
Sakib008 <mdshakib1902@gmail.com>
2025-12-15 14:46:21 +0530

    feat : added home page ui components

---
+++ src/components/ui/NoiseOverlay.tsx
@@ -0,0 +1,16 @@
+export default function NoiseOverlay() {
+  return (
+    <div className="pointer-events-none fixed inset-0 z-[9999] h-full w-full opacity-[0.03]">
+      <svg className="h-full w-full">
+        <filter id="noiseFilter">
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.6"
+            stitchTiles="stitch"
+          />
+        </filter>
+        <rect width="100%" height="100%" filter="url(#noiseFilter)" />
+      </svg>
+    </div>
+  );
+}
