b984db10201d050d96e805c06440b7a65ba173ea
Sakib008 <mdshakib1902@gmail.com>
2025-12-20 21:21:59 +0530

    fix : remotePattern

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
   images: {
-    domains: ["images.pexels.com",'drive.google.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+      },
+    ],
   },
 };
 
